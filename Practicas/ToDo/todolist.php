5f982427e94e40bd9e14de9a9556608c05f3557f
cactus <correa.francisco.21@gmail.com>
2014-09-04 20:44:05 -0300

    Lista de Tareas (con querys y while autoincrementa + lista htmll)

--- Practicas/ToDo/todolist.php
+++ Practicas/ToDo/todolist.php
@@ -1,18 +1,23 @@
 <?php
+//Funcion que devuelve las tareas del usuario 'Juanito'
+function Query($nomUsuario){
 $mysql = mysqli_connect("localhost", "admin", "admin", "todolist");
-$res = mysqli_query($mysql, "SELECT * from tareas WHERE usuarioID = '2'");
+$res = mysqli_query($mysql, "SELECT * FROM usuarios AS u INNER JOIN tareas AS t ON u.usuarioID = t.usuarioID WHERE u.usuarioID =2");
+$row = mysqli_fetch_assoc($res);
+
+//Lista con items marcando la tarea
+echo '<ul>';
+while ($row !== NULL) {
+   echo '<li>'.($row['nomTarea']).'</li>';
+   echo "\n";
+   $row = mysqli_fetch_assoc($res);
+}
+echo '</ul>';
+/**$res = mysqli_query($mysql, "SELECT * from tareas WHERE usuarioID = '4'");
 $row = mysqli_fetch_assoc($res);
 echo '<pre>'.var_dump($row).'<pre>';
-echo "\n";
-
-while ($row !== NULL) {
-    echo '<pre>'.var_dump($row).'<pre>';
-    echo "\n";
-    $row = mysqli_fetch_assoc($res);
+*/
 }
 
-$res = mysqli_query($mysql, "SELECT * from tareas WHERE usuarioID = '4'");
-$row = mysqli_fetch_assoc($res);
-echo '<pre>'.var_dump($row).'<pre>';
-
+Query('juanito')
 ?>
