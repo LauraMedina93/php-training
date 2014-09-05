e4bc03c0c0ca6188939326c1c82c1a0155f84096
cactus <correa.francisco.21@gmail.com>
2014-09-05 20:19:17 -0300

    funciones de consulta, insercion de usuarios y tareas a la base de datos

--- Practicas/ToDo/todolist.php
+++ Practicas/ToDo/todolist.php
@@ -10,7 +10,7 @@ function Query($nomUsuario) {
     echo '<h1>'.'Tareas de: ' .'<b>'. $row['nombre'].'</b></h1>';
     echo "\n";
     echo '<ol>';
-    while ($row !== NULL) {
+    while ($row !== NULL) { //imprime hasta conocer todos los datos del array
         echo '<h3>'.'<li>' . ($row['nomTarea']) . '</li></h3>';
         echo "\n";
         $row = mysqli_fetch_assoc($res);
@@ -24,3 +24,37 @@ function Query($nomUsuario) {
 
 Query('juanito')
 ?>
+
+<html>
+    <head> <h2> Agregar un Nuevo Usuario</h2> </head>
+
+<body>
+
+    
+    <?php
+    //funcion que inserta un usuario nuevo
+    function nuevoUsuario($usuarioNuevo) {
+        $mysql = mysqli_connect("localhost", "admin", "admin", "todolist"); //conexion con la bd
+        $res2 = mysqli_query($mysql, "INSERT INTO `usuarios`(nombre) VALUES ('$usuarioNuevo')"); //ingreso de usuario
+
+        echo 'Se agrego el usuario' . $res2['nombre'];// imprime el texto y el nombre ingresado
+        if (!$res2) {
+            echo mysqli_error($mysql);//imprime el error en caso de no cargarse el usuario
+        }
+    }
+    //funcion que inserta una tarea nueva
+    function insertar($tareaNueva) {
+        $mysql = mysqli_connect("localhost", "admin", "admin", "todolist");//conexion con la bd
+        $res3 = mysqli_query($mysql, "INSERT INTO `tareas`(`nomTarea`, `estado`, `usuarioID`) VALUES ($nomTarea,$estado,$usuarioID)"); //ingreso de tarea por medio de una query
+        $row = mysqli_fetch_assoc($res);//no es necesario, solo si se hace un 'select'
+        if (!$res3) {
+            echo mysqli_error($mysql);
+        }
+    }
+
+    nuevoUsuario('dario')
+    ?>
+
+
+</body>
+</html>
