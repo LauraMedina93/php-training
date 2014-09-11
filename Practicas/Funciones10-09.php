8c4330d527d8442fa1047102ba849da80af53a0f
cactus <correa.francisco.21@gmail.com>
2014-09-11 18:34:26 -0300

    Funciones anonimas y obtencion de datos de un input

--- Practicas/Funciones10-09.php
+++ Practicas/Funciones10-09.php
@@ -1,33 +1,41 @@
-<?php
-session_start();
-if(isset($_POST['name']) && isset($_POST['password'])){
-$_SESSION['name'] = $_POST['name'];
-$_SESSION['pass'] = $_POST['password'];
-} else {
-    echo 'Error! Faltan completar campos';
-}
+<html>
+    <head> jQuery </head>
+    <body
+    <?php
+    session_start();
+    if (isset($_POST['name']) && isset($_POST['password'])) {
+        $_SESSION['name'] = $_POST['name'];
+        $_SESSION['pass'] = $_POST['password'];
+    } else {
+        echo 'Error! Faltan completar campos';
+    }
+
 //funcion anonima
-function myfunction(){
-    return 'result';
-}
-$result = myfunction();
-echo(function(){
-    return 'result';
-});
-//funcion de carga de input
-$(function (){
-    $("#name").keyup (function (){
-        var inputName = $("#name"); //aca se llama al id name desde jquery
-        var nameLength = inputName.val().length; //val() muestra todo lo que se ingresa en el input
-        if (nameLength >=13) {
-            inputName.CSS ('background-color','FFFAAA');
-        } else {
-            inputName.CSS ('background-color','#FFFFFF');
-        }
+    function myfunction() {
+        return 'result';
+    }
+
+    $result = myfunction();
+    echo(function() {
+        return 'result';
     });
-    $("#name").blur(function (){
-    var nameLength
-        
-    });
-});
-?>
+    ?>
+    <script>
+        $(function() {
+            $("#name").keyup(function() {
+                var inputName = $("#name"); //aca se llama al id name desde jquery
+                var nameLength = inputName.val().length; //val() muestra todo lo que se ingresa en el input
+                if (nameLength >= 13) {
+                    inputName.CSS('background-color', 'FFFAAA');
+                } else {
+                    inputName.CSS('background-color', '#FFFFFF');
+                }
+            });
+            $("#name").blur(function() {
+                var nameLength
+
+            });
+        });
+        </script>
+    </body>
+</html>
