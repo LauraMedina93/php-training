38f4224fcf62cff410ad901b86e8a4534d467f7f
Usuario <Usuario@Laura>
2014-11-13 00:23:15 -0300

    alert with javascript

--- practice/province-app/js/main.js
+++ practice/province-app/js/main.js
@@ -1,6 +1,24 @@
-
-
-$(function(){
-    alert('I am here!');
-})();
-
+$(document).ready(function(){
+        var consulta;                                                  
+         //hacemos focus al campo de búsqueda
+        $("#text").focus();                                                                                           
+        //comprobamos si se pulsa una tecla
+        $("#text").keyup(function(e){
+                                     
+              //obtenemos el texto introducido en el campo de búsqueda
+              consulta = $("#text").val();
+                                                                           
+              //hace la búsqueda
+                                                                                  
+              $.ajax({
+                    type: "POST",
+                    url: "index.php",
+                    data: consulta,
+                    dataType: "html",
+                   
+                    success:function(){
+                         alert ("Usted ingreso: " + consulta);
+                    },      
+              });                                                     
+        });                                                  
+});
