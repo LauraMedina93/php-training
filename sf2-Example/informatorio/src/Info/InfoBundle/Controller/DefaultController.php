60fa738b1b6c18c7a920394ebbafac91ad899a26
cactus <correa.francisco.21@gmail.com>
2014-12-12 19:22:39 -0300

    Clase 12/12

--- sf2-Example/informatorio/src/Info/InfoBundle/Controller/DefaultController.php
+++ sf2-Example/informatorio/src/Info/InfoBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Info\InfoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Info\InfoBundle\Entity\Post;
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -17,4 +18,24 @@ class DefaultController extends Controller
     {
         return $this->render('InfoInfoBundle:Default:fake.html.twig');
     }
+    
+    /**
+     * @Route("/info/add/{name}", name="_info_add")
+     * @Template()
+     */
+    
+    public function doctrineAction($name)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = new Post;
+        $post->setDescription($name);
+        $post->setDate(new \DateTime());
+        $em->persist($post);
+        $em->flush();
+        
+        return $this->render('InfoInfoBundle:Default:doctrine.html.twig',
+                array('object' => $post->getDescription()));
+    }
+    
+   
 }
